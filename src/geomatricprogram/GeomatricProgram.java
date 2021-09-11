12f7a313f6663c8736bd3f689d3c4571b7c798b5
mudable <mudableshilpa@gmail.com>
2021-09-11 12:55:21 +0530

    Added:Length

--- src/geomatricprogram/GeomatricProgram.java
+++ src/geomatricprogram/GeomatricProgram.java
@@ -1,7 +1,20 @@
 package geomatricprogram;
 
+import java.util.Scanner;
+
 public class GeomatricProgram {
-public static void main(String[] args) {
-	System.out.println("Welcome to Line Comparison Computation Program");
+	double x1,x2,y1,y2;
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+		System.out.println("Welcome to Line Comparison Computation");
+		Scanner scan=new Scanner(System.in);
+	      System.out.println("enter the values:");
+	      double x1=scan.nextDouble();
+	      double x2=scan.nextDouble();
+	      double y1=scan.nextDouble();
+	      double y2=scan.nextDouble();
+		double len=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+	    System.out.println("length is:"+len);
+
 }
 }
