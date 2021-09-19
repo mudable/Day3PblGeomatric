46eaf0a85dc7257bac538f5d9da443498f153e6f
mudable <mudableshilpa@gmail.com>
2021-09-19 18:33:53 +0530

    Added:UC4 to check equality and compare them by using cmp and equ method

--- src/geomatricprogram/GeomatricProgram.java
+++ src/geomatricprogram/GeomatricProgram.java
@@ -2,36 +2,43 @@ package geomatricprogram;
 
 import java.util.Scanner;
 
-public class GeomatricProgram {
-	double x1,x2,y1,y2;
-	double a1,a2,b1,b2;
+public class GeomatricProgram{
+	static int x1, a1;
+	static int x2, a2;
+	static int y1, b1;
+	static int y2, b2;
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Computation Program");
-		Scanner scan=new Scanner(System.in);
-	      System.out.print("enter the values:");
-	      double x1=scan.nextDouble();
-	      double x2=scan.nextDouble();
-	      double y1=scan.nextDouble();
-	      double y2=scan.nextDouble();
-	      System.out.print("enter the values:");
-	      double a1=scan.nextDouble();
-	      double a2=scan.nextDouble();
-	      double b1=scan.nextDouble();
-	      double b2=scan.nextDouble();
-		double line1=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-		double line2=Math.sqrt((a2-a1)*(a2-a1)+(b2-b1)*(b2-b1));
-	    System.out.println("length is:"+line1);
-	    System.out.println("length is:"+line2);
-	    if (line1 == line2) {
+		System.out.println("enter the values");
+		Scanner sc = new Scanner(System.in);
+		double x1 = sc.nextDouble();
+		double x2 = sc.nextDouble();
+		double y1 = sc.nextDouble();
+		double y2 = sc.nextDouble();
+
+		System.out.print("enter the values:");
+		double a1 = sc.nextDouble();
+		double a2 = sc.nextDouble();
+		double b1 = sc.nextDouble();
+		double b2 = sc.nextDouble();
+		double line1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		String x = Double.toString(line1);
+		double line2 = Math.sqrt((a2 - a1) * (a2 - a1) + (b2 - b1) * (b2 - b1));
+		String y = Double.toString(line2);
+		System.out.println("length is:" + line1);
+		System.out.println("length is:" + line2);
+		System.out.println(x.equals(y));
+		int comp = x.compareTo(y);
+		
+		if (comp == 0) {
 			System.out.println("lines are equal");
+		} else if (line1 > line2) {
+			System.out.println("Line1 is greater");
+		} else {
+
+			System.out.println("Line2 is lesser than Line1");
 		}
-	    else if(line1>line2) {
-	    	System.out.println("Line1 is greater");
-	    }
-	    else {
-	    	
-	    	System.out.println("Line2 is lesser than Line1");
-	    }
-		}
+	}
 }
 
